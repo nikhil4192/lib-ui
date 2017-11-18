61ac0d6b92db9002a02cf699537ee83a29bebd3b
Nikhil <nikhil4192@gmail.com>
2017-11-18 13:17:26 -0500

    Login and signup static pages.

---
+++ src/app/com-lib-signup/com-lib-signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-com-lib-signup',
+  templateUrl: './com-lib-signup.component.html',
+  styleUrls: ['./com-lib-signup.component.css']
+})
+export class ComLibSignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
