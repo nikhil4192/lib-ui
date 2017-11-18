9d55d427819a24e462bfbb5eaccc659ed95f0845
Nikhil <nikhil4192@gmail.com>
2017-11-18 18:54:50 -0500

    Search, profile and history components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { AppComponent } from './app.component';
 import { ComLibLoginComponent } from './com-lib-login/com-lib-login.component';
 import { ComLibSignupComponent } from './com-lib-signup/com-lib-signup.component';
 import { UserHomeComponent } from './user-home/user-home.component';
+import { SearchBookComponent } from './search-book/search-book.component';
+import { HistoryComponent } from './history/history.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const appRoutes: Routes = [
-  { path: 'signin', component: ComLibLoginComponent},
-  { path: 'signup', component: ComLibSignupComponent},
-  { path: 'userhome', component: UserHomeComponent}
+  { path: 'signin', component: ComLibLoginComponent },
+  { path: 'signup', component: ComLibSignupComponent },
+  { path: 'userhome', component: UserHomeComponent},
+  { path: 'searchBook', component: SearchBookComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
@@ -19,12 +25,18 @@ const appRoutes: Routes = [
     AppComponent,
     ComLibLoginComponent,
     ComLibSignupComponent,
-    UserHomeComponent
+    UserHomeComponent,
+    SearchBookComponent,
+    HistoryComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AlertModule.forRoot(),
     RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
