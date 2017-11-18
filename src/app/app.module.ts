27b401bc506b0cfe363f29598b7fbe75403e37d2
Nikhil <nikhil4192@gmail.com>
2017-11-18 15:23:37 -0500

    Routing established between the pages and a new component created for user home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ComLibLoginComponent } from './com-lib-login/com-lib-login.component';
 import { ComLibSignupComponent } from './com-lib-signup/com-lib-signup.component';
+import { UserHomeComponent } from './user-home/user-home.component';
+
+const appRoutes: Routes = [
+  { path: 'signin', component: ComLibLoginComponent},
+  { path: 'signup', component: ComLibSignupComponent},
+  { path: 'userhome', component: UserHomeComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ComLibLoginComponent,
-    ComLibSignupComponent
+    ComLibSignupComponent,
+    UserHomeComponent
   ],
   imports: [
     BrowserModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
